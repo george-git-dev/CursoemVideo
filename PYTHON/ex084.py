ddaec7710cd350a4ecf5ef2dad1ad0f5d35f6daf
George-Silva-GIT <george_roots@hotmail.com>
2021-06-23 20:14:01 -0300

    Exercícios Python

--- PYTHON/ex084.py
+++ PYTHON/ex084.py
@@ -1,15 +1,31 @@
-expr = str(input("Digite a expressão: "))
-pilha = []
-for simb in expr:
-    if simb == "(":
-        pilha.append("(")
-    elif simb == ")":
-        if len(pilha) > 0:
-            pilha.pop()
-        else:
-            pilha.append(")")
-            break
-if len(pilha) == 0:
-    print("Sua expressão está válida!")
-else:
-    print("Sua expressão está errada!")
+temp = []
+principal = []
+maior = 0
+menor = 0
+while True:
+    temp.append(str(input("Nome: ")))
+    temp.append(float(input("Peso: ")))
+    if len(principal) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    principal.append(temp[:])
+    temp.clear()
+    resp = str(input("Quer continuar ? [S/N] "))
+    if resp in "Nn":
+        break
+print("-=" * 30)
+print(f"Ao todo, você cadastrou {len(principal)}")
+print(f"O maior peso foi de {maior} kg ", end="")
+for p in principal:
+    if p[1] == maior:
+        print(f"[{p[0]}] ", end="")
+print()
+print(f"O menor peso foi de {menor} kg. Peseo de ", end="")
+for p in principal:
+    if p [1] == menor:
+        print(f"[{p[0]}] ", end="")
+print()
